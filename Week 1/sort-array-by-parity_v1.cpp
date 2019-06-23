f3561d5d4f617b633e138f56d4f4371577aeccc6
donatih55 <donatihenrique@hotmail.com>
2019-06-23 15:22:00 -0500

    Update sort-array-by-parity_v1.cpp

--- Week 1/sort-array-by-parity_v1.cpp
+++ Week 1/sort-array-by-parity_v1.cpp
@@ -4,8 +4,7 @@ public:
         int originalSize = A.size();
         for (int i = 0; i < originalSize; i++){
             if(A.at(i)%2 == 1){
-                A.push_back(A.at(i));
-                A.erase(A.begin() + i);
+                swap(A.at(i),A.at(originalSize - 1));
                 i--;
                 originalSize--;
             }
