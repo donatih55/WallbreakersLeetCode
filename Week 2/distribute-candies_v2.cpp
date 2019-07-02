71f4309514069e4aa4779ed0969ea4033d8b8856
donatih55 <donatihenrique@hotmail.com>
2019-07-01 20:28:05 -0500

    distribute-candies

---
+++ Week 2/distribute-candies_v2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int distributeCandies(vector<int>& candies) {
+        unordered_map<int,int> distinctCandiesCount;
+        //add the candy to the map and increment count if it is already there
+        for (int i = 0; i < candies.size(); i++){
+            if (distinctCandiesCount.find(candies.at(i)) != distinctCandiesCount.end()){
+                distinctCandiesCount[candies.at(i)]++;
+            }else{
+                distinctCandiesCount[candies.at(i)] = 1;
+            }
+            // if there's more types of candies than types to give to the kids return the max candy a kid can get
+            if (distinctCandiesCount.size() > candies.size()/2)
+                return candies.size()/2;
+        }
+        return distinctCandiesCount.size();
+    }
+};
